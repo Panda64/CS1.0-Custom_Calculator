7084409d29c0bc432cc82faf26fe73dc18228035
Panda64 <JaylenSchelb@yahoo.com>
2020-08-30 20:40:36 -0700

    Added introduction print statements

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
 # Name - Jaylen Schelb
 # Program - BMI Calculator 
 
+print("Your Body Mass Index, or BMI, is a measure of your body fat based off of your height and weight. \
+    A normal BMI is anywhere between 18.5-24.9.")
+print("To start, enter how many feet tall you are. We will get to the inches in a second.")
