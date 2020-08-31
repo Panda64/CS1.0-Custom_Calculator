71405a4ca37e6a3f254b6e74c378bc7a39a8daea
Panda64 <JaylenSchelb@yahoo.com>
2020-08-31 11:56:18 -0700

    Added user input for height

--- program.py
+++ program.py
@@ -2,5 +2,25 @@
 # Program - BMI Calculator 
 
 print("Your Body Mass Index, or BMI, is a measure of your body fat based off of your height and weight. \
-    A normal BMI is anywhere between 18.5-24.9.")
+A normal BMI is anywhere between 18.5-24.9.")
 print("To start, enter how many feet tall you are. We will get to the inches in a second.")
+
+def user_feet():
+    while True:
+        try:
+            return int(input())
+        except ValueError:
+            print("You must enter a whole number! Try again.")                       
+
+feet = user_feet()
+print("Great! Now enter the remaining inches")
+
+def user_inches():
+    while True:
+        try:
+            return int(input())
+        except ValueError:
+            print("You must enter a whole number! Try again.")
+
+inches = user_inches()
+print(f"{feet} feet and {inches} inches!")
